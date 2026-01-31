96ec1811ac6aa0852556cd6b395136a9146e4db8
sutti <suttiponglaisiri@gmail.com>
2026-01-31 22:16:34 +0700

    create line chart

--- src/App.java
+++ src/App.java
@@ -1,19 +1,49 @@
 import javafx.application.Application;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.text.Font;
+import javafx.scene.chart.LineChart;
+import javafx.scene.chart.NumberAxis;
+import javafx.scene.chart.XYChart;
+//import javafx.scene.control.Label;
+//import javafx.scene.layout.BorderPane;
+//import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
 public class App extends Application {
     public void start(Stage window){
-        BorderPane main = new BorderPane();
-        Label name = new Label("Hellow");
-        //main.getChildren().add(name);
-        name.setFont(Font.font("Arial",60));
-        main.setCenter(name);
-        Scene scene = new Scene(main, 700, 800);
-        window.setTitle("Test JavFx");
+    
+        //create X axis and Y charts
+        NumberAxis xAxis = new NumberAxis(2000, 2020, 5);
+        NumberAxis yAxis = new NumberAxis(40, 80, 4);
+
+        //label for title
+        xAxis.setLabel("Year");
+        yAxis.setLabel("The University of Helsinki Ranking");
+
+        //create line chart with xAxis and yAxis
+        LineChart<Number, Number> lineChart = new LineChart<>(xAxis, yAxis);
+        lineChart.setTitle("Shanghai Ranking Consultancy");
+
+        //create data for add to line chart
+        XYChart.Series<Number,Number> rankData = new XYChart.Series<>();
+        rankData.setName("Hel");
+
+        rankData.getData().add(new XYChart.Data<>(2007, 73));
+        rankData.getData().add(new XYChart.Data<>(2008, 68));
+        rankData.getData().add(new XYChart.Data<>(2009, 72));
+        rankData.getData().add(new XYChart.Data<>(2010, 72));
+        rankData.getData().add(new XYChart.Data<>(2011, 74));
+        rankData.getData().add(new XYChart.Data<>(2012, 73));
+        rankData.getData().add(new XYChart.Data<>(2013, 76));
+        rankData.getData().add(new XYChart.Data<>(2014, 73));
+        rankData.getData().add(new XYChart.Data<>(2015, 67));
+        rankData.getData().add(new XYChart.Data<>(2016, 56));
+        rankData.getData().add(new XYChart.Data<>(2017, 56));
+
+        //add data to line chart
+        lineChart.getData().add(rankData);
+
+        Scene scene = new Scene(lineChart, 640, 480);
+        //window.setTitle("Test JavFx");
         window.setScene(scene);
         window.show();
     }
